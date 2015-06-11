e4e536ab2c67472ef63efeba6945d9a49abe9924
Mark Wilkinson <markawil@yahoo.com>
2015-06-10 21:25:06 -0500

    initial commit, first day of work

---
+++ YahtzeeSwift/NavController.swift
@@ -0,0 +1,26 @@
+//
+//  NavController.swift
+//  YahtzeeSwift
+//
+//  Created by Mark Wilkinson on 6/9/15.
+//  Copyright (c) 2015 Mark Wilkinson. All rights reserved.
+//
+
+import UIKit
+
+class NavController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationBar.translucent = false;
+        self.navigationBar.barTintColor = UIColor.blueColor()
+        self.navigationBar.tintColor = UIColor.whiteColor()
+        let fontDictionary = [ NSForegroundColorAttributeName:UIColor.whiteColor(),
+            NSFontAttributeName:UIFont(name: "Avenir-Heavy", size: 20.0)!]
+        self.navigationBar.titleTextAttributes = fontDictionary
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+}
---
+++ YahtzeeSwift/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  YahtzeeSwift
+//
+//  Created by Mark Wilkinson on 6/9/15.
+//  Copyright (c) 2015 Mark Wilkinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Yahtzee Swift";
+    }
+}
+
